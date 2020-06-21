9296eeb2298607cfbc69a809f63a7820fb6a1ac8
Maciej Kożuszek <mackozuszek@gmail.com>
2020-06-21 22:30:26 +0200

    Prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hidden">hidden div</div>
+      <Observer>
+        <div>show</div>
+      </Observer>
     </div>
   );
 }
+
+const Observer = ({ children }) => {
+  const [setRef, visible] = useIntersect({ threshold: 0.5 });
+  console.log(visible);
+
+  return (
+    <div ref={setRef} className={`${visible ? "show" : "hidden"}`}>
+      {children}
+    </div>
+  );
+};
+
+const useIntersect = (options) => {
+  const [ref, setRef] = useState(null);
+  const [visible, setVisible] = useState(false);
+  console.log(ref);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, [ref, options]);
+
+  return [setRef, visible];
+};
 
 export default App;
